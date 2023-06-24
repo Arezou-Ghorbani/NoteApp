f0dafc0b59c6c1f72a15818de13f8fc9616a1f32
arz.ghorbani <Agjhgjhg@gmail.com>
2023-06-24 11:43:15 +0330

    manifest and main activity updated

--- app/src/main/java/com/example/noteapp/room/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/noteapp/room/ui/main/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.noteapp.room.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.noteapp.R
+import com.example.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Binding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //initViews
+        binding.apply {
+
+        }
     }
 }
